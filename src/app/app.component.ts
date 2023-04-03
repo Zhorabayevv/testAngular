a15da4f13645a4b7fa46545a2d20af14e9fcafb2
Zhorabaevv <nzhorabaev777@gmail.com>
2023-04-03 21:27:26 +0600

    shdfg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'testAngular';
+  @ViewChild('element') elementRef: ElementRef;
+
+  showContextMenu = false;
+  contextMenuPosition: { top: number; left: number };
+  selectedItem: string;
+
+  onRightClick(event: MouseEvent): void {
+    event.preventDefault();
+    this.showContextMenu = true;
+    this.contextMenuPosition = { top: event.clientY, left: event.clientX };
+  }
+
+  onMenuItemClick(value: string): void {
+    const el = this.elementRef.nativeElement;
+    el.focus();
+    const selection = window.getSelection();
+    if (!selection) return;
+
+    const range = selection.getRangeAt(0);
+    const node = document.createElement('span');
+    node.innerHTML = value;
+    range.insertNode(node);
+
+    el.textContent = el.innerHTML;
+
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+
+  insertHtmlAtCursor(html: string): void {
+    const el = this.elementRef.nativeElement;
+    const selection = window.getSelection();
+    if (selection.rangeCount === 0) return;
+
+    const range = selection.getRangeAt(0);
+    const node = range.createContextualFragment(html);
+
+    range.deleteContents();
+    range.insertNode(node);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 }
